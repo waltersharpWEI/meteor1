6bac643854fb1888c9252b53a448591bfd5948b6
waltersharp <waltersharp@163.com>
2019-07-03 17:04:42 +0800

    third

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,30 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
 import { Tasks } from '../api/tasks.js';
 
+import './task.js';
 import './body.html';
 
 Template.body.helpers({
   tasks() {
-    return Tasks.find({});
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    event.preventDefault();
+    const target = event.target;
+    const text = target.text.value;
+   
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
+    });
+   
+    target.text.value= '';
   },
 });
