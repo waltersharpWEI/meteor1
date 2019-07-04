d70824eac020ff1adcfc2a455c0a14a7fdde060a
waltersharp <waltersharp@163.com>
2019-07-04 10:23:08 +0800

    5

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,9 +6,20 @@ import { Tasks } from '../api/tasks.js';
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
   tasks() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      return Tasks.find({ checked: { $ne: true} }, {sort: {createdAt:-1} });
+    }
     return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
   },
 });
 
@@ -26,5 +37,8 @@ Template.body.events({
     });
    
     target.text.value= '';
+  },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 });
